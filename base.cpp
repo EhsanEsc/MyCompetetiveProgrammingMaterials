0bad1d6c05d5f361c9e29e784870613a1afe7837
EhsanEscandari <ehsan.escandari@ut.ac.ir>
2021-11-05 18:32:35 +0330

    Add base.cpp

---
+++ base.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define pr(x) cerr << #x << ": " << x << endl
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+
+const int N = 2e5+22; 
+const ll INF = 2e17+22;
+
+int main()
+{ IOS;
+    
+
+    return 0;
+}
